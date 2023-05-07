5ff260e9428bd0481ef9aed64612ac62b7ecbc99
Kalyan <kalyan.ben10@live.com>
2023-05-07 10:34:52 +0000

    printf - go

---
+++ language-playground/go/course_codes/go_bootcamp/4.print/3.false_claims.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	claim := false
+	fmt.Printf("These are %t claims.\n", claim)
+}
---
+++ language-playground/go/course_codes/go_bootcamp/4.print/4.temp.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	temp := 29.5
+	fmt.Printf("Temperature is %.2f degrees.", temp)
+}
