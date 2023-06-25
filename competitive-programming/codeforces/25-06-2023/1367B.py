ea5e568ec1b348d95a0a06709c827f1e63713440
kalyanr <kalyan.ben10@live.com>
2023-06-25 21:33:34 +0530

    temp

---
+++ competitive-programming/codeforces/25-06-2023/1367B.py
@@ -0,0 +1,16 @@
+# 1367B.py
+
+T = int(input())
+
+for _ in range(T):
+    l = int(input())
+    lst = map(int, input().split())
+    for i in range(l):
+        lst[i] = lst[i] % 2
+    for i in lst:
+        
+        
+        
+    
+            
+            
