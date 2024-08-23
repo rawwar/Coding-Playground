a967cbc944ea9400c882d91a0f979a8259be3124
kalyanr <kalyan.ben10@live.com>
2024-08-23 19:36:04 +0530

    add usaco

---
+++ CompetitiveProgramming/USACO/general/basketBallOneOne.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kalyan reddy kasireddy on 23/08/24.
+//
+
+#include "../usaco_utils.h"
+
+void solution()
+{
+    string s;
+    cin >> s;
+    cout << s[s.length() - 2];
+
+}
---
+++ CompetitiveProgramming/USACO/usaco_utils.h
@@ -0,0 +1,17 @@
+//
+// Created by kalyan reddy kasireddy on 23/08/24.
+//
+
+#ifndef USACO_UTILS_H
+#define USACO_UTILS_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#endif //USACO_UTILS_H
